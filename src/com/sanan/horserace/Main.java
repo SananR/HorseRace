52e45ebb1801350c00bef25c3d938e99974b0868
Sanan Rao <ninjaboy323@hotmail.com>
2017-10-02 19:14:09 -0600

    Maven setup
    Setup maven

--- src/com/sanan/horserace/Main.java
+++ src/com/sanan/horserace/Main.java
@@ -1,10 +1,14 @@
 package com.sanan.horserace;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	public static Plugin plugin;
+	
 	public void onEnable() {
+		plugin = this;
 		saveDefaultConfig();
 	}
 	
